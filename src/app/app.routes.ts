3840f3596ecf040fac8d0471f911a1a33ad6ab5d
Vamsi Krishna Manda <krishnaradha8118@gmail.com>
2024-08-06 15:05:57 +0530

    Basic Template

---
+++ src/app/app.routes.ts
@@ -0,0 +1,15 @@
+import { Routes } from '@angular/router';
+import { ServicesComponent } from './services/services.component';
+import { AboutComponent } from './about/about.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const routes: Routes = [
+    {path:"services",component:ServicesComponent},
+    {path:"about",component:AboutComponent},
+    {path:"contact",component:ContactComponent},
+    {path:"signup",component:SignupComponent},
+    {path:"login",component:LoginComponent}
+
+];
